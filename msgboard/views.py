06eccc888f61d05ed320b776154b09daf9574e44
Ofek Manor <ofekm3000@gmail.com>
2023-02-22 18:41:32 +0200

    Added some read-only view functions and templates
    - Added some read-only view functions and templates
    to show how the data would be presented
    
    - changed setup.sh to run the migrations
    as part of the process starting up

--- msgboard/views.py
+++ msgboard/views.py
@@ -1,3 +1,9 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+from .models import Message
+
+
+def board(request):
+    messages = Message.objects.order_by('-date')
+    return render(request, 'msgboard/board.html', {'messages': messages})
 
 # Create your views here.
